002da7e1737f4f7375bc3c533ab6c47c997082ce
Alla <allamaley@gmail.com>
2017-12-06 20:00:14 +0100

    First version

---
+++ js/myscript.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+	//fading in the first part
+	$('.paral').fadeIn(1000);
+// Add soft angle-down scroll
+  $("#angle-down").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+//end soft  angle-down scroll
+// Add soft  angle-up scroll
+  $("#angle-up").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 700, function(){
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+//end soft scroll
+//scrollspy nav
+$('body').scrollspy({ target: '#nav' })
+//end of scrollspy nav
+//Switch fa icon arrow in accordion
+function toggleIcon(e) {
+        $(e.target)
+            .prev('.card-header')
+            .find("i")
+            .toggleClass('fa-rotate-180');
+    }
+    $('#accordion').on('hidden.bs.collapse', toggleIcon);
+    $('#accordion').on('shown.bs.collapse', toggleIcon);
+//end of switching
+
+});
